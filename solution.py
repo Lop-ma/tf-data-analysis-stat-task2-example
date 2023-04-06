2f2bbac8ec376b120b535e8a97e79308a27e9d48
Lop-ma <55141590+Lop-ma@users.noreply.github.com>
2023-04-06 23:53:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,5 +10,5 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     df = pd.DataFrame(data=x)
     chi_l, chi_r = chi2.ppf([1-alpha/2, alpha/2], df = 1)
-    return np.sqrt(df[0].var(ddof=0) / chi_l / 20) , \
-           np.sqrt(df[0].var(ddof=0) / chi_r / 20)
+    return df[0].var(ddof=0) / chi_l / 20 , \
+           df[0].var(ddof=0) / chi_r / 20
